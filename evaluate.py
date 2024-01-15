fb293b11c762422c81d276a90280296bbb976c1f
Expertium <83031600+Expertium@users.noreply.github.com>
2024-01-15 04:57:32 +0300

    Updated the confidence intervals using method='BCa'.md (#35)
    Co-authored-by: Jarrett Ye <jarrett.ye@outlook.com>

--- evaluate.py
+++ evaluate.py
@@ -2,24 +2,52 @@ import pathlib
 import json
 import numpy as np
 import scipy
+import math
 
 dict_x_w = None
-            
+
+
 def weighted_mean(z, axis):
     # creating an array of weights, by mapping z to dict_x_w
     data = np.vectorize(dict_x_w.get)(z)
     return np.average(data[0], weights=data[1], axis=axis)
 
+def sigdig(value, CI):
+    def num_lead_zeros(x):
+        return math.inf if x == 0 else -math.floor(math.log10(abs(x))) - 1
+
+    def first_nonzero_digit(x):
+        x = str(x)
+        for digit in x:
+            if digit == "0" or digit == ".":
+                pass
+            else:
+                return int(digit)
+
+    n_lead_zeros_CI = num_lead_zeros(CI)
+    CI_sigdigs = min(len(str(CI)[2 + n_lead_zeros_CI:]), 2)
+    decimals = n_lead_zeros_CI + CI_sigdigs
+    rounded_CI = round(CI, decimals)
+    first_sigdig_CI = first_nonzero_digit(rounded_CI)
+    if first_sigdig_CI<5:
+        rounded_value = round(value, decimals - 1)
+        return str(f'{rounded_value:.{decimals - 1}f}'), str(f'{rounded_CI:.{decimals}f}')
+    else:
+        rounded_value = round(value, max(decimals - 2, 0))
+        rounded_CI = round(CI, max(decimals - 1, 1))
+        return str(f'{rounded_value:.{max(decimals - 2, 0)}f}'), str(f'{rounded_CI:.{max(decimals - 1, 1)}f}')
+
+
 if __name__ == "__main__":
     for model in (
-        "FSRS-4.5",
-        "FSRS-rs",
-        "FSRSv4",
-        "FSRS-4.5-dry-run",
-        "SM2",
-        "FSRSv3",
-        "LSTM",
-        "HLR",
+            "FSRS-4.5",
+            "FSRS-rs",
+            "FSRSv4",
+            "FSRS-4.5-dry-run",
+            "SM2",
+            "FSRSv3",
+            "LSTM",
+            "HLR",
     ):
         print(f"Model: {model}")
         m = []
@@ -44,17 +72,30 @@ if __name__ == "__main__":
             values = np.array([item[metric] for item in m])
             identifiers = [i for i in range(len(values))]
             dict_x_w = {identifier: (value, weight) for identifier, (value, weight) in enumerate(zip(values, sizes))}
-            CI_99_bootstrap = scipy.stats.bootstrap((identifiers,), statistic=weighted_mean, confidence_level=0.99, axis=0, method='BCa')
+            CI_99_bootstrap = scipy.stats.bootstrap((identifiers,), statistic=weighted_mean, confidence_level=0.99,
+                                                    axis=0, method='BCa')
             low = list(CI_99_bootstrap.confidence_interval)[0]
             high = list(CI_99_bootstrap.confidence_interval)[1]
-            print(f"{model} mean (n_reviews): {np.average(values, weights=sizes):.4f}")
-            print(f"99% CI of {metric} (n_reviews), bootstrapping (scipy): {(high - low) / 2:.4f}")
-            dict_x_w = {identifier: (value, np.log(weight)) for identifier, (value, weight) in enumerate(zip(values, sizes))}
-            CI_99_bootstrap = scipy.stats.bootstrap((identifiers,), statistic=weighted_mean, confidence_level=0.99, axis=0, method='BCa')
-            low = list(CI_99_bootstrap.confidence_interval)[0]
-            high = list(CI_99_bootstrap.confidence_interval)[1]
-            print(f"{model} mean (ln(n_reviews)): {np.average(values, weights=np.log(sizes)):.4f}")
-            print(f"99% CI of {metric} (ln(n_reviews)), bootstrapping (scipy): {(high - low) / 2:.4f}")
+            CI = (high - low) / 2
+            wmean = np.average(values, weights=sizes)
+            rounded_mean, rounded_CI = sigdig(wmean, CI)
+            # asymm = (high - wmean)/(wmean - low)
+            print(f"{model} mean (n_reviews): {rounded_mean}")
+            print(f"99% CI of {metric} (n_reviews), bootstrapping (scipy): {rounded_CI}")
+            # print(f"Asymmetry of the CI={asymm:.4f}")
+            dict_x_w = {identifier: (value, np.log(weight)) for identifier, (value, weight) in
+                        enumerate(zip(values, sizes))}
+            CI_99_bootstrap = scipy.stats.bootstrap((identifiers,), statistic=weighted_mean, confidence_level=0.99,
+                                                    axis=0, method='BCa')
+            low2 = list(CI_99_bootstrap.confidence_interval)[0]
+            high2 = list(CI_99_bootstrap.confidence_interval)[1]
+            CI2 = (high2 - low2) / 2
+            wmean2 = np.average(values, weights=np.log(sizes))
+            rounded_mean2, rounded_CI2 = sigdig(wmean2, CI2)
+            # asymm2 = (high2 - wmean2)/(wmean2 - low2)
+            print(f"{model} mean log(n_reviews): {rounded_mean2}")
+            print(f"99% CI of {metric} log(n_reviews), bootstrapping (scipy): {rounded_CI2}")
+            # print(f"Asymmetry of the CI={asymm2:.4f}")
 
         try:
             rmse_bin_again = np.array([item["RMSE(bins)Ratings"]["1"] for item in m])
@@ -67,3 +108,4 @@ if __name__ == "__main__":
 
         if len(weights) > 0:
             print(f"weights: {np.median(weights, axis=0).round(4).tolist()}")
+        print('')

fbe801dcd15e97c693fb8251343ccef36aaebf63
Jarrett Ye <jarrett.ye@outlook.com>
2024-12-23 16:39:36 +0800

    update recency weighting in script.py

--- script.py
+++ script.py
@@ -275,9 +275,8 @@ def process(user_id):
             try:
                 train_partition = train_set[train_set["partition"] == partition].copy()
                 if RECENCY:
-                    train_partition["weights"] = np.linspace(
-                        0.5, 1.5, len(train_partition)
-                    )
+                    x = np.linspace(0, 1, len(train_partition))
+                    train_partition["weights"] = 0.25 + 0.75 * np.power(x, 3)
                 if DRY_RUN:
                     partition_weights[partition] = optimizer.init_w
                     continue

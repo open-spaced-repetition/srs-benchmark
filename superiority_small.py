d813cb86bc75cabbc3f24cd6316ce647f6242bba
Jarrett Ye <jarrett.ye@outlook.com>
2025-04-18 09:47:50 +0800

    remove FSRSv3 & SM2 & RMSE-BINS-EXPLOIT

--- superiority_small.py
+++ superiority_small.py
@@ -20,13 +20,10 @@ if __name__ == "__main__":
         "FSRSv4",
         "DASH",
         "NN-17",
-        "FSRSv3",
         "ACT-R",
         "HLR",
         "Ebisu-v2",
         "Anki-dry-run",
-        "SM2",
-        "RMSE-BINS-EXPLOIT",
     ]
     csv_name = f"{len(models)} models.csv"
 
@@ -108,13 +105,11 @@ if __name__ == "__main__":
     index_lstm = models.index("LSTM-short-secs-equalize_test_with_non_secs")
     index_anki_dry_run = models.index("Anki-dry-run")
     index_v4 = models.index("FSRSv4")
-    index_v3 = models.index("FSRSv3")
     index_Ebisu_v2 = models.index("Ebisu-v2")
     index_FSRS_6_recency = models.index("FSRS-6-recency")
     models[index_lstm] = "LSTM"
     models[index_anki_dry_run] = "Anki-SM-2\ndef. param."
     models[index_v4] = "FSRS v4"
-    models[index_v3] = "FSRS v3"
     models[index_Ebisu_v2] = "Ebisu v2"
     models[index_FSRS_6_recency] = "FSRS-6\nrecency"
 
